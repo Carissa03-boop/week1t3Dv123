b7fb2c425d7a3941fe7141b64d48115cc884da6a
Carissa03-boop <125882806+Carissa03-boop@users.noreply.github.com>
2023-08-08 09:35:36 +0200

    week2revision

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,29 @@
 // When the document loads
-$(document).ready[function(){
+$(document).ready(function(){
 
-    console.log("Hello");
+    console.log("hello")
 
-}];
+
+    // ------------------------------------------------------------------
+    //  Home Page
+
+    //When the Hero Image Loads, Animate it Upwards
+    $(".hero-image").animate({top:'-=100px'});
+
+    // ------------------------------------------------------------------
+    //  Browse Page 
+    $("#descriptionText").hide();
+
+});
+
+// When the card is clicked
+
+$(".card").click(function(){
+    // Toggle Price and Description text
+    $("#priceText").toggle();
+    $("#descriptionText").toggle();
+
+    // Resize the image to fit additional content
+    $(".card-img-top").toggleClass("small");
+    
+})
